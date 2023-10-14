e8b9946b415d92b2acc030452d1a0d6d8faa0446
Gracilliano <gracillianowp@hotmail.com>
2023-10-14 01:44:07 -0300

    :hammer: #3 - criada a chave estrangeira entre a tabela events com a tabela users

--- database/migrations/2023_10_14_042723_create_events_table.php
+++ database/migrations/2023_10_14_042723_create_events_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->text('description');
             $table->date('date');
             $table->string('hour', 5);
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
