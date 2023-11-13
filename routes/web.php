940569313e855761ae6a9153b0123a7360c1c299
christian-lelis <chistiantest@gmail.com>
2023-11-12 21:06:56 -0300

    #15 tela de histórico concluida

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,9 @@
 <?php
 
 use App\Http\Controllers\EventsController;
+use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('index');
@@ -30,5 +20,6 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('events', EventsController::class);
 });
+Route::get('/hist', [HistoryController::class, 'index'])->name('hist.index');
 
 require __DIR__.'/auth.php';
