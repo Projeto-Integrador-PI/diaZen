c9a8be104e1167437e5c1a53c28b36723371f107
Gracilliano Carvalho <gracillianowp@hotmail.com>
2023-11-15 22:33:57 -0300

    :fire: #38 - criada a interação básica entre o dashboard e os formulários de evento e sentimentos

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $dateNow = Carbon::now();
+        setlocale(LC_TIME, 'Brazil');
+        return view('dashboard.index', [
+            'dateNow' => $dateNow->format('d/m/Y'),
+            'dayWeek' => $dateNow->format('l'),
+            'dates' => []
+        ]);
+    }
+}
